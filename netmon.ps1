193238c65906f4ad5446b0dd7ea85be2b379bbd0
Bimal Singh <bimalsingh983@gmail.com>
2020-10-02 13:48:37 +0530

    Update netmon.ps1

--- netmon.ps1
+++ netmon.ps1
@@ -1,5 +1,6 @@
+$TempDir = [System.IO.Path]::GetTempPath()
 $url = "http://networklookout.com/dwn/nmemplpro_agent.msi"
-$output = "nmemplpro_agent.msi"
+$output = $TempDir+"nmemplpro_agent.msi"
 $start_time = Get-Date
 
 Import-Module BitsTransfer
@@ -9,4 +10,4 @@ Start-BitsTransfer -Source $url -Destination $output
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-msiexec /i nmemplpro_agent.msi PASSWORD=myAgentPassword /qn
+msiexec /i $TempDir + "nmemplpro_agent.msi" PASSWORD=myAgentPassword /qn
