1fc09cd5881231cf5825632e116f08ac80fb97f8
Bimal Singh <bimalsingh983@gmail.com>
2020-10-02 13:22:51 +0530

    Add files via upload

---
+++ netmon.ps1
@@ -0,0 +1,10 @@
+$url = "http://networklookout.com/dwn/nmemplpro_agent.msi"
+$output = "nmemplpro_agent.msi"
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+#OR
+#Start-BitsTransfer -Source $url -Destination $output -Asynchronous
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
